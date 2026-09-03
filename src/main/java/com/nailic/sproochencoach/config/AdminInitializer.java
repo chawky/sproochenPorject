086f3bcc3c9ed5a93881d1ad0858ac8175bfd307
chawky <neilychawky2@gmail.com>
2026-09-03 23:26:27 +0200

    feat : admin model full feature

---
+++ src/main/java/com/nailic/sproochencoach/config/AdminInitializer.java
@@ -0,0 +1,46 @@
+package com.nailic.sproochencoach.config;
+
+import com.nailic.sproochencoach.model.AppRole;
+import com.nailic.sproochencoach.model.AppUser;
+import com.nailic.sproochencoach.repository.AppUserRepo;
+import com.nailic.sproochencoach.repository.RoleRepo;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@RequiredArgsConstructor
+public class AdminInitializer implements CommandLineRunner {
+
+    private final AppUserRepo userRepo;
+    private final RoleRepo roleRepo;
+
+    @Value("${app.admin.email:}")
+    private String adminEmail;
+
+    @Override
+    @Transactional
+    public void run(String... args) {
+
+        if (adminEmail == null || adminEmail.isBlank()) {
+            return;
+        }
+
+        AppUser user = userRepo.findByEmail(adminEmail)
+                .orElse(null);
+
+        if (user == null) {
+            return;
+        }
+
+        AppRole adminRole = roleRepo.findByName("ADMIN");
+        if (adminRole == null) {
+            throw new IllegalStateException("ADMIN role does not exist");
+        }
+        user.getRoles().add(adminRole);
+        userRepo.save(user);
+    }
+}
